1db14e4d5162575780b954df27d5a005261ecadc
Hui Kang <rocksnow1942@gmail.com>
2019-10-02 09:01:43 -0700

    add

--- app/APPS/__init__.py
+++ app/APPS/__init__.py
@@ -3,3 +3,17 @@ from flask import Blueprint
 bp = Blueprint('apps', __name__,)  # template_folder='templates'
 
 from app.APPS import routes
+
+
+
+#NOTE
+# to use session id secret
+# run bokeh sercret to get a key.
+# then before run bokeh server,
+#
+#export BOKEH_SECRET_KEY=jpwLZikCcKlokJtvjMjmSwhe06po51SGpeCcbNbbKEJj
+#export BOKEH_SIGN_SESSIONS=1
+#bokeh serve  --session-ids external-signed simuojo plojo ... 
+#also set flask environment variable with the same 
+#BOKEH_SECRET_KEY=jpwLZikCcKlokJtvjMjmSwhe06po51SGpeCcbNbbKEJj
+#BOKEH_SIGN_SESSIONS=1
