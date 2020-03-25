d10a38f11949c11bfc796689ad4b36362c2da268
rocksnow1942 <rocksnow1942@gmail.com>
2020-03-25 14:33:12 -0700

    a

--- app/tasks/pptmonitor_windows/ppt_monitor_windows.py
+++ app/tasks/pptmonitor_windows/ppt_monitor_windows.py
@@ -7,49 +7,98 @@ from watchdog.events import PatternMatchingEventHandler
 from pptx import Presentation
 from pathlib import PureWindowsPath
 from comtypes import client
-from ctypes import Structure, windll, c_uint, sizeof, byref
 from datetime import datetime
 import gc
 import requests
-import logging 
+import logging
 from logging.handlers import RotatingFileHandler
-
-
+import win32api
+import psutil
 #C:\Users\aptitude\Anaconda3\pythonw.exe "C:\Users\aptitude\Aptitude-Cloud\R&D\Users\Hui Kang\Scripts\ppt_monitor.py"
 
 source_folder = r"C:\Users\aptitude\Aptitude-Cloud\R&D\Projects"
 target_folder = r"C:\Users\aptitude\Aptitude-Cloud\R&D Backup\Plojo backup\Project_Slide_snap"
 log_file = r"C:\Users\aptitude\Aptitude-Cloud\R&D Backup\Plojo backup\Project_Slide_snap\log.json"
-running_log = r"C:\Users\aptitude\Aptitude-Cloud\R&D\Users\Hui Kang\Scripts\ppt_monitor\monitor_log.txt"
-printscreen = False
+running_log = r"C:\Users\aptitude\Desktop\monitorlogs\monitor_log.txt"
+
 post_url = 'http://192.168.86.200/pptmonitor_port'
 
 
-class LASTINPUTINFO(Structure):
-    _fields_ = [
-        ('cbSize', c_uint),
-        ('dwTime', c_uint),
-    ]
+class LASTINPUTINFO():
+    def __init__(self):
+        self.update()
+
+    def update(self):
+        self.lastinput = win32api.GetLastInputInfo()
+        self.time = time.time()
+
+    def __call__(self, *args, **kwargs):
+        recentinput = win32api.GetLastInputInfo()
+        if recentinput == self.lastinput:
+            dt = time.time()-self.time
+        else:
+            dt = (recentinput - self.lastinput) / 1000
+            dt = time.time()-self.time - dt
+            self.update()
+        return dt
 
 
-def get_idle_duration():
-    lastInputInfo = LASTINPUTINFO()
-    lastInputInfo.cbSize = sizeof(lastInputInfo)
-    windll.user32.GetLastInputInfo(byref(lastInputInfo))
-    millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
-    return millis / 1000.0
+get_idle_duration = LASTINPUTINFO()
+
+
+def delete_folder_content(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
 def make_snapshot(source, target):
     powerpoint = client.CreateObject('Powerpoint.Application')
     powerpoint.Presentations.Open(source)
     powerpoint.WindowState = 2
-    if not os.path.isdir(target):
+    if not os.path.isdir(target.strip()):
         os.makedirs(target.strip())
+    delete_folder_content(target.strip())
     powerpoint.ActivePresentation.Export(target.strip()+'.pptx', 'PNG')
     powerpoint.ActivePresentation.Close()
     powerpoint.Quit()
     return None
+
+
+def checkIfProcessRunning(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False
+
+
+def close_office(logger):
+    try:
+        if checkIfProcessRunning('WINWORD.exe'):
+            os.system("TASKKILL /F /IM WINWORD.exe")
+            logger.debug("close word attempted.")
+        if checkIfProcessRunning('EXCEL.exe'):
+            os.system("TASKKILL /F /IM EXCEL.exe")
+            logger.debug("close excel attempted.")
+        if checkIfProcessRunning("chrome.exe"):
+            os.system("TASKKILL /F /IM chrome.exe")
+            logger.debug("close chrome attempted.")
+
+    except Exception as e:
+        logger.error("close_office ERROR: "+str(e))
 
 
 def glob_pptx(path):
@@ -74,7 +123,7 @@ def get_revision(*files):
 
 
 class FileLogger():
-    def __init__(self, source_folder="", target_foler="", log_file="", running_log="",loglevel='INFO'):
+    def __init__(self, source_folder="", target_foler="", log_file="", running_log="", loglevel='INFO'):
         self.data = {}
         self.revisions = {}
         self.source_folder = source_folder
@@ -96,21 +145,13 @@ class FileLogger():
         for i in ['debug', 'info', 'warning', 'error', 'critical']:
             setattr(self, i, getattr(self.logger, i))
 
-    def write_log(self, content):
-        if printscreen:
-            print(content)
-        with open(self.running_log, 'a') as f:
-            f.write(f"{self.time} - " + content+'\n')
-        return 0
-
     def post(self, msg):
         try:
-            a=requests.post(url=post_url, json={
-                          'time': self.time, 'msg': msg}, timeout=(10, 30))
+            a = requests.post(url=post_url, json={
+                'time': self.time, 'msg': msg}, timeout=(10, 30))
             self.debug(f'Post answer is : {a.text}')
         except Exception as e:
             self.debug(f'Post failed: {e}')
-            
 
     @property
     def time(self):
@@ -144,7 +185,7 @@ class FileLogger():
             self.info('Success: Init Revision Done.')
         except Exception as e:
             self.error(f'Error: {e}')
-        return 
+        return
 
     def create(self, file):
         self.data[file] = 'create'
@@ -204,7 +245,7 @@ class FileLogger():
             unresolved.update(json.load(f))
         with open(self.log_file+'_errors', 'wt') as f:
             json.dump(unresolved, f, indent=2)
-        return 
+        return
 
 
 class PPTX_Handler(PatternMatchingEventHandler):
@@ -213,8 +254,6 @@ class PPTX_Handler(PatternMatchingEventHandler):
                          ignore_directories=False, case_sensitive=True)
         self.logger = logger
         self.info = logger.info
-
-  
 
     def on_created(self, event):
         self.info(f"Watchdog: Create {event.src_path}")
@@ -240,7 +279,7 @@ class PPTX_Handler(PatternMatchingEventHandler):
 def main(loglevel='INFO'):
     my_observer = Observer()
     logger = FileLogger(source_folder=source_folder,
-                        target_foler=target_folder, log_file=log_file, running_log=running_log,loglevel=loglevel)
+                        target_foler=target_folder, log_file=log_file, running_log=running_log, loglevel=loglevel)
     logger.info('File Monitor Lanunch')
     logger.post("PPTmonitor is running.")
     logger.init_revision()
@@ -251,18 +290,22 @@ def main(loglevel='INFO'):
     gc.collect()
     while True:
         try:
-            time.sleep(60)
+            time.sleep(90)
             try:
+                logger.debug('Idle duration {}'.format(get_idle_duration()))
                 if get_idle_duration() > 120:
                     logger.sync_snap()
+                    logger.debug('Sync attempt.')
                 # post signal every 1 minutes
+                if get_idle_duration() > 900:
+                    close_office(logger)
                 logger.post("PPTmonitor is running.")
             except Exception as e:
                 logger.error(f'PPTmonitor make snap shot loop: {e}')
             gc.collect()
         except Exception as e:
             logger.critical(f'PPTmonitor Loop Break: {e}')
-            time.sleep(60)
+            time.sleep(160)
 
 
 if __name__ == '__main__':
