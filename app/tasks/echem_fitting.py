40d73394aae0432e35eb4232eaf0e6e136470041
rocksnow1942 <rocksnow1942@gmail.com>
2020-06-06 18:13:32 -0700

    ON: Sat Jun  6 18:13:31 2020, FROM: mini

--- app/tasks/echem_fitting.py
+++ app/tasks/echem_fitting.py
@@ -1,8 +1,7 @@
-import numpy
+import numpy as np
 from scipy import signal, optimize
 from app import create_app
 from app.plojo_models import Plojo_Data
-from datetime import datetime
 from app import db
 
 app = create_app(keeplog=False)
@@ -16,11 +15,14 @@ def add_echem_pstrace(amsid, data):
     potential = data.get('potential', None)
     amp = data.get('amp', None)
     if potential and amp:
-        xydataIn = numpy.array([potential, amp])
-        res = fitpeak(xydataIn)
-        peakcurrent = round(float(res[5]), 6)
+        try:
+            xydataIn = numpy.array([potential, amp])
+            res = fitpeak(xydataIn)
+            peakcurrent = round(float(res[5]), 6)
+        except:
+            peakcurrent = -100 # to indicate fitting error
     else:
-        peakcurrent = -100  # to indicate fitting error
+        peakcurrent = -50  # to indicate missing value
 
     plojodata = Plojo_Data.query.get(amsid) 
     plojodata_data = plojodata.data
@@ -43,7 +45,8 @@ def add_echem_pstrace(amsid, data):
     plojodata.data = plojodata_data 
     db.session.commit()
 
-def fitpeak(xydataIn, nstd=3.5, peakwidth=0.25, minpot=-1, maxpot=0.1):
+
+def fitpeak(xydataIn, nstd=3.1, peakwidth=0.25, minpot=-1, maxpot=0.1):
     '''Gives me the peak current from a scan
     fits the data to a polynomial and a gaussian, then subtracts the
     xydataIn this is the current and voltage data set for one scan
@@ -59,11 +62,11 @@ def fitpeak(xydataIn, nstd=3.5, peakwidth=0.25, minpot=-1, maxpot=0.1):
     x = xydataIn[0, :]  # voltage
     y = xydataIn[1, :]  # current
 
-    # Cut out the bad data
-    truncx, truncy = truncEdges(x, y, minpot, maxpot)
+    # Cut out the bad data; didn't cut since the old code is not doing it.
+    truncx, truncy = x, y
 
     # Normalize the current by the average current value
-    norm = numpy.mean(truncy)
+    norm = truncy.mean()
     truncy = truncy/norm
 
     # Apply butterfilter
@@ -71,8 +74,8 @@ def fitpeak(xydataIn, nstd=3.5, peakwidth=0.25, minpot=-1, maxpot=0.1):
     fs = 1/voltageInterval  # Sampling rate
     fc = 0.1*fs  # Cut off frequency ***We made up this scale factor based on limited emperical data***
 
-    filt_b, filt_a = signal.butter(1, fc/(fs/2))
-    truncy = signal.lfilter(filt_b, filt_a, truncy)
+    sos = signal.butter(1, fc, btype='lowpass', output='sos', fs=fs)
+    truncy = signal.sosfilt(sos, truncy)
 
     # Override values
     nstd = 4.5  # 4 is good
@@ -96,17 +99,17 @@ def fitpeak(xydataIn, nstd=3.5, peakwidth=0.25, minpot=-1, maxpot=0.1):
     edge = 0.015  # Emperically chosen cut off value in Volts
 
     for i in range(0, len(centerLocs)):
-        if (truncx[centerLocs[i]] > (max(truncx)-edge)) or (truncx[centerLocs[i]] < (min(truncx)+edge)):
+        if (truncx[centerLocs[i]] > ((truncx.max())-edge)) or (truncx[centerLocs[i]] < ((truncx.min())+edge)):
             centerLocs[i] = 0
     peakHeights = peakProps["peak_heights"]
     peakwidths = peakProps["widths"]
     # Need to cut out all elements peak height, width, centerlocs where we are outside of edge
-    peakHeights = numpy.delete(
+    peakHeights = np.delete(
         peakHeights, [i for i in range(len(centerLocs)) if centerLocs[i] == 0])
-    peakwidths = numpy.delete(
-        peakwidths, [i for i in range(len(centerLocs)) if centerLocs[i] == 0])
-    centerLocs = numpy.delete(
-        centerLocs, [i for i in range(len(centerLocs)) if centerLocs[i] == 0])
+    peakwidths = np.delete(peakwidths, [i for i in range(
+        len(centerLocs)) if centerLocs[i] == 0])
+    centerLocs = np.delete(centerLocs, [i for i in range(
+        len(centerLocs)) if centerLocs[i] == 0])
 
     peakHeight = 0
     if(len(peakHeights) > 0):
@@ -138,65 +141,47 @@ def fitpeak(xydataIn, nstd=3.5, peakwidth=0.25, minpot=-1, maxpot=0.1):
         fiterror = -0.001  # increase so that it can be seen
         return xnotjunk, ynotjunk, xforfit, gauss, baseline, peakcurrent, peakvoltage, fiterror
     else:
-        baseMask = []
-        peakMask = []
-        xbase = []
-        ybase = []
-
-        # Now we find the regions of the data for determining the baseline and the reduction peak (peak)
-        for i in range(0, len(truncx)):
-            # Gets the portion of the data that's (1/4 of the peakwidth) either side of the center. 1/8 samples more of the peak, 1/2.5 samples less of the peak
-            baseMask.append((truncx[i] > (center + peakwidth/2.5))
-                            or (truncx[i] < (center - peakwidth/2.5)))
-
-            # Gets the portion aroud the center (1/2 peak width) that's the gaussian
-            peakMask.append((truncx[i] > (center - peakwidth/1.5))
-                            and (truncx[i] < (center + peakwidth/1.5)))
-
-            # Isolates the data to consider for the baseline fit
-            if baseMask[i] and peakMask[i]:
-                # Finds the intersection of these to consider for the baseline fit
-                xbase.append(truncx[i])
-                # Corresponding y data
-                ybase.append(truncy[i])
+        # this is to get the base part of the peak, that is -PW/1.5 ~ - PW/2.5 , PW/2.5 ~ PW/1.5; this peakwidth is weirdly scaled.
+        peakandbaseMask = np.logical_and(np.logical_or(truncx < (center - peakwidth/2.5), truncx > (center + peakwidth/2.5)),
+                                         np.logical_and(truncx > (center - peakwidth/1.5), truncx < (center + peakwidth/1.5)))
+        xbase = truncx[peakandbaseMask]
+        ybase = truncy[peakandbaseMask]
 
         # Get liner least squares fit for baseline data
-        vp = numpy.polyfit(xbase, ybase, 1)
+        vp = np.polyfit(xbase, ybase, 1)
 
         # Get reasonable guesses for non-linear regression
         # give reasonable starting values for non-linear regression
-        v0 = [vp[0], vp[1], numpy.log(
+        v0 = [vp[0], vp[1], np.log(
             peakHeight-linFit(vp, center))[0], center[0], peakwidth/6]
 
         # Run minimization of fitting error to get optimal values for the fitting parameters v
         v = optimize.fmin(getFillFitError, v0, (truncx, truncy), disp=False)
         fiterrornorm = getFillFitError(v, truncx, truncy)
 
-        ip = numpy.subtract(gaussAndLinFit(v, [v[3]]), linFit(v, v[3]))
+        ip = gaussAndLinFit(v, v[3]) - linFit(v, v[3])
         potp = v[3]
 
-        peakMask = []
-        for i in range(0, len(truncx)):
-            peakMask.append((truncx[i] > (v[3] - nstd*v[4]))
-                            and (truncx[i] < (v[3] + nstd*v[4])))
+        # peak mask that is number of SD away.
+        peakMask = np.logical_and(
+            truncx > (v[3] - nstd*v[4]), truncx < (v[3] + nstd*v[4]))
 
-        truncxMasked = x[[i for i in range(
-            len(peakMask)) if peakMask[i] == True]]
+        truncxMasked = x[peakMask]
         full = gaussAndLinFit(v, truncxMasked)
         base = linFit(v, truncxMasked)
 
         # Outputs
         xnotjunk = truncx
-        ynotjunk = numpy.dot(truncy, norm)
+        ynotjunk = truncy * norm
         xforfit = truncxMasked
-        gauss = numpy.dot(full, norm)
-        baseline = numpy.dot(base, norm)
-        peakcurrent = numpy.dot(ip, norm)
+        gauss = full * norm
+        baseline = base * norm
+        peakcurrent = ip * norm
         peakvoltage = potp
         fiterror = fiterrornorm*norm*100
         if len(gauss) == 0:
             xnotjunk = truncx
-            ynotjunk = numpy.dot(truncy, norm)
+            ynotjunk = truncy * norm
             xforfit = 0
             gauss = 0
             baseline = 0
@@ -216,35 +201,6 @@ def linFit(v, x):
     return y
 
 
-def truncEdges(x, y, min, max):
-    '''
-        Removes outlier portion of fit of known bad signal from a scan data
-    '''
-
-    # Eliminate the values that are outside of the desired range
-    truncxOutTemp = numpy.zeros(len(x))  # init lists
-    truncyOutTemp = numpy.zeros(len(x))
-
-    count = -1
-    for j in range(0, len(x)):
-        if x[j] >= min and x[j] <= max:
-            count += 1
-            truncxOutTemp[count] = x[j]
-            truncyOutTemp[count] = y[j]
-
-    # Trim the padded zeros off the vectors
-    truncxOut = numpy.zeros(count)
-    truncyOut = numpy.zeros(count)
-
-    for j in range(0, count):
-        truncxOut[j] = truncxOutTemp[j]
-        truncyOut[j] = truncyOutTemp[j]
-
-    truncxOut = numpy.transpose(truncxOut)
-    truncyOut = numpy.transpose(truncyOut)
-    return truncxOut, truncyOut
-
-
 def gaussAndLinFit(v, x):
     '''
         Gaussian on a lienar baseline
@@ -253,11 +209,7 @@ def gaussAndLinFit(v, x):
            Fitting parameters are handed by the arrary (v),
            where v = [slope, y-intecept, Gaussian scalar, Gaussian mean, SD]    
     '''
-    y = []
-    for i in range(len(x)):
-        y.append(v[0]*x[i] + v[1] + numpy.exp(v[2]) *
-                 numpy.exp(-(((x[i]-v[3])**2)/(2*v[4]**2))))
-    return y
+    return v[0]*x + v[1] + np.exp(v[2])*np.exp(-(((x-v[3])**2)/(2*v[4]**2)))
 
 
 def gaussNormFit(v, x):
@@ -268,11 +220,7 @@ def gaussNormFit(v, x):
        Fitting parameters are handed by the arrary (v),
        where v = [y-intecept, slope, Gaussian scalar, Gaussian mean, SD]
     '''
-    y = []
-    for i in range(len(x)):
-        if(numpy.exp(-(((x[i]-v[3])**2)/(2*v[4]**2))) != None):
-            y.append(numpy.exp(-(((x[i]-v[3])**2)/(2*v[4]**2))))
-    return y
+    return np.exp(-(((x-v[3])**2)/(2*v[4]**2)))
 
 
 def getFillFitError(v, x, y):
@@ -281,30 +229,27 @@ def getFillFitError(v, x, y):
         deviations (nSD) of the peak in a weighted manner
     '''
     nstd = 4.5
-    peakMask = []
-    baseMask = []
-    for i in range(0, len(x)):
-            peakMask.append((x[i] > (v[3] - 2.5*v[4]))
-                            and (x[i] < (v[3] + 2.5*v[4])))
-            baseMask.append((x[i] > (v[3] - nstd*v[4])) and (x[i]
-                                                             < (v[3] + nstd*v[4])) and (not peakMask[i]))
 
-    xbase = x[[i for i in range(len(baseMask)) if baseMask[i] == True]]
-    ybase = y[[i for i in range(len(baseMask)) if baseMask[i] == True]]
-    subGaussAndLin = numpy.subtract(gaussAndLinFit(v, xbase), ybase)
-    error_vector = subGaussAndLin/numpy.sqrt(sum(baseMask))
+    peakMask = np.logical_and(x > (v[3] - 2.5*v[4]), x < (v[3] + 2.5*v[4]))
+    baseMask = np.logical_or(np.logical_and(x > (v[3] - nstd*v[4]), x < (v[3] - 2.5*v[4])),
+                             np.logical_and(x > (v[3] + 2.5*v[4]), x < (v[3] + nstd*v[4])))
 
-    # Peak error is weighted by exp(-distance from peak)
-    xpeak = x[[i for i in range(len(peakMask)) if peakMask[i] == True]]
-    ypeak = y[[i for i in range(len(peakMask)) if peakMask[i] == True]]
-    subGaussAndLin = numpy.subtract(gaussAndLinFit(v, xpeak), ypeak)
-    multGuassNorm = gaussNormFit(v, xpeak)
-    productArray = numpy.multiply(
-        subGaussAndLin/numpy.sqrt(sum(peakMask)), multGuassNorm)
+    normMask = gaussNormFit(v, x) * peakMask + baseMask
 
-    error_vector = numpy.concatenate((error_vector, productArray))
+    return (((gaussAndLinFit(v, x) - y) * normMask) ** 2 / (peakMask.sum() + baseMask.sum())).sum()
 
-    for i in range(len(error_vector)):
-        error_vector[i] = error_vector[i]**2
-    sse = sum(error_vector)
-    return sse
+    # This Following code is a strict implement of the original code,
+    # but I think it's more reasonable to use the number of both peak and base as error denominator.
+    # xbase = x[baseMask]
+    # ybase = y[baseMask]
+    # subGaussAndLin = gaussAndLinFit(v, xbase) - ybase
+    # error_vector = subGaussAndLin/np.sqrt(baseMask.sum())
+    #
+    # # Peak error is weighted by exp(-distance from peak)
+    # xpeak = x[peakMask]
+    # ypeak = y[peakMask]
+    # subGaussAndLin = gaussAndLinFit(v, xpeak)-ypeak
+    # multGuassNorm = gaussNormFit(v,xpeak)
+    # productArray = subGaussAndLin/np.sqrt(peakMask.sum()) * multGuassNorm
+    #
+    # return (error_vector ** 2).sum() + (productArray**2).sum()
